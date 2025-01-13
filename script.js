460909e3f6916a33dea5534cd3e48be8414e59e6
Nicole Fernandez Sanchez <n.fernandez17@unapec.edu.do>
2025-01-13 13:31:51 -0400

    add tasks to local storage and display them to the DOM

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ const addBtn = document.querySelector('.add-task');
 // const checkBox = document.getElementById('checkbox');
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+//function to display tasks from local storage
+function displayTasks(){
+    const tasksFromStorage = getTasksFromStorage();
 
-//function to add a task (to DOM for the moment)
-function addTask(e){
+    tasksFromStorage.forEach(task => {
+        addTaskToDOM(task);
+    });
+    checkUI();
+}
+//handler function that calls the function to add tasks to the DOM and local storage
+function onAddItemSubmit(e){
     e.preventDefault();
 
     const newTask = taskInput.value;
@@ -23,24 +31,86 @@ function addTask(e){
         alert('Please, add a task');
         return;
     }
-    console.log('success'); 
 
-    //create li element
-    const li = document.createElement('li');
-    let checkbox = createCheckbox(newTask);
-    li.appendChild(checkbox);
-    const deleteIcon = createTrash();
-    li.appendChild(deleteIcon);
+    //add task to DOM
+    addTaskToDOM(newTask);
 
-    //append li to ul
-    taskList.appendChild(li);
+    //add task to local storage
+    addTaskToStorage(newTask);
     taskInput.value = '';
     // console.log(li);
     checkUI();
     
 }
+//el parametro es el texto de la tarea
+function addTaskToDOM(task){
+    
+    //create li element
+    const li = document.createElement('li');
+    let checkbox = createCheckbox(task);
+    li.appendChild(checkbox);
+    const deleteIcon = createTrash();
+    li.appendChild(deleteIcon);
 
+    //append li to ul (D0M)
+    taskList.appendChild(li);
+}
+
+//function to add tasks to local storage
+function addTaskToStorage(task){
+    const tasksFromStorage = getTasksFromStorage();
+
+    //add the new task to the array
+    tasksFromStorage.push(task);
+
+    //convert to JSON string and set to local storage
+    localStorage.setItem('tasks', JSON.stringify(tasksFromStorage));
+
+}
+
+//function to get tasks from local storage returns an array if there are tasks, otherwise an empty array
+function getTasksFromStorage(){
+    let tasksFromStorage;
+
+    if(localStorage.getItem('tasks') === null){
+        tasksFromStorage = [];
+    }else{
+        // originalmente nos devuelve un string, aqui lo convertimos a un array
+        tasksFromStorage = JSON.parse(localStorage.getItem('tasks'));   
+    }
+    return tasksFromStorage;
+}
+
+//===============================================================================================
+//function to add a task (to DOM for the moment)
+// function addTask(e){
+//     e.preventDefault();
+
+//     const newTask = taskInput.value;
+//     if(newTask === ''){
+//         alert('Please, add a task');
+//         return;
+//     }
+//     console.log('success'); 
+
+//     //create li element
+//     const li = document.createElement('li');
+//     let checkbox = createCheckbox(newTask);
+//     li.appendChild(checkbox);
+//     const deleteIcon = createTrash();
+//     li.appendChild(deleteIcon);
+
+//     //append li to ul
+//     taskList.appendChild(li);
+//     taskInput.value = '';
+//     // console.log(li);
+//     checkUI();
+    
+// }
+//===============================================================================================
 //function create checkbox
+
+
 function createCheckbox(text){
     const div = document.createElement('div');
     div.classList.add('text-checkbox');
@@ -165,19 +235,25 @@ function checkUI(){
 // console.log(document.querySelector('li').firstChild.textContent);
 // console.log(document.querySelector('li > .text-checkbox').textContent.trim());
 // console.log(document.querySelector('li').firstElementChild.textContent.trim());
+init();
+//function to initialize app
 
-
-checkUI();
-// console.log(document.querySelector('li').firstElementChild.textContent);
-
-// Event Listeners
-taskForm.addEventListener('submit', addTask);
-taskList.addEventListener('click', removeTask);
-clearBtn.addEventListener('click', clear);
-filter.addEventListener('input', filterTasks);
-// checkboxes.addEventListener('click', checkCheckBox);
-document.addEventListener('DOMContentLoaded',checkUI);
-checkboxes.forEach(checkbox =>{
-    checkbox.addEventListener('change', checkCheckBox)
-});
+function init(){
+    checkUI();
+    
+    
+    // Event Listeners
+    taskForm.addEventListener('submit', onAddItemSubmit); //anadirlo en el dom y en local storage
+    // taskForm.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clear);
+    filter.addEventListener('input', filterTasks);
+    // checkboxes.addEventListener('click', checkCheckBox);
+    document.addEventListener('DOMContentLoaded',checkUI);
+    checkboxes.forEach(checkbox =>{
+        checkbox.addEventListener('change', checkCheckBox)
+    });
+    document.addEventListener('DOMContentLoaded', displayTasks);
+    
+}
 
